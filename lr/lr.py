65d53ade6de1181482a285a6b9873958b53e8838
naihai‘git config --global user.email 448435279@qq.comgit clone git@github.zhfsky.com:naihai/watermarker.gitcd watermarklstouch REAMDEMDgit config --global user.name naihai‘ <448435279@qq.com>
2019-07-23 10:55:51 +0800

    增加逻辑回归Softmax 多分类

--- lr/lr.py
+++ lr/lr.py
@@ -5,9 +5,11 @@
 import numpy as np
 import math
 from utils import accuracy_score, normalize
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
 
 """
-LR 二项分类
+LR Sigmoid二项分类 与Softmax 多项分类
 """
 
 
@@ -17,32 +19,60 @@ class LogisticRegression:
         self.reg = reg
         self.multi_class = multi_class
         self.learning_rate = learning_rate
-        self.params = None  # w 向量
+        self.W = None  # w 向量 N 多分类时为矩阵 CxD
 
-    def _initialize_weights(self, n_features):
+    def _initialize_weights(self, n_features, n_classes=2):
         """
         初始化权重向量
         :param n_features:
         :return:
         """
         limit = 1.0 / math.sqrt(n_features)
-        self.params = np.random.uniform(-limit, limit, (n_features,))
+        if self.multi_class:
+            self.W = np.random.uniform(-limit, limit, (n_classes, n_features))
+        else:
+            self.W = np.random.uniform(-limit, limit, (n_features,))
 
-    def _loss_and_gradient(self, X, y):
+    def _loss_and_gradient_sigmoid(self, X, y):
         """
+        二分类 sigmoid
         使用GD计算梯度与损失
+        :param X: NxD  m_samples, n_features
+        :param y: N
+        :return:
+        """
+        m_samples, n_features = X.shape
+        y_pred = sigmoid(X.dot(self.W))
+        # 计算梯度
+        gradient = - 1.0 / m_samples * X.T.dot(y - y_pred) + self.reg * self.W
+        # 计算loss
+        loss = - 1.0 / m_samples * np.sum(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))  # 经验风险损失
+        loss = loss + 0.5 * self.reg * np.sum(self.W * self.W)  # 结构风险损失
+        return loss, gradient
+
+    def _loss_and_gradient_softmax(self, X, y):
+        """
+        多分类 softmax
         :param X:
         :param y:
         :return:
         """
         m_samples, n_features = X.shape
-        y_pred = sigmoid(X.dot(self.params))
-        gradient = X.T.dot(y_pred - y)
-        gradient = 1.0 / m_samples * gradient + self.reg * self.params  # 均值 正则化
-        # 计算loss
-        loss = np.sum(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))  # 经验风险损失
-        # 结构风险损失
-        loss = - 1.0 / m_samples * loss + 0.5 * self.reg * self.params * self.params
+
+        scores = X.dot(self.W.T)  # 矩阵NxC  每个样本在C个类上的预测分数
+        # 经过Softmax转换为概率
+        scores_exp = np.exp(scores - np.max(scores, axis=-1, keepdims=True))  # 防止溢出 减去最大值 因此全部小于等于0
+        scores_exp_sum = np.sum(scores_exp, axis=-1, keepdims=True)  # 每个样本在C个类上的预测概率之和 归一化
+
+        # 每个样本真实的标签对应的softmax概率
+        y_label_softmax = scores_exp[range(m_samples), y].reshape((scores_exp.shape[0], 1)) / scores_exp_sum
+        # 计算损失
+        loss = - 1.0 / m_samples * np.sum(np.log(y_label_softmax))
+        loss = loss + 0.5 * self.reg * np.sum(self.W * self.W)
+
+        y_pred = scores_exp / scores_exp_sum
+
+        gradient = -1.0 / m_samples * X.T.dot(y_label_softmax - y_pred).T + self.reg * self.W
         return loss, gradient
 
     def fit(self, X, y, n_iterations):
@@ -54,26 +84,35 @@ class LogisticRegression:
         :return:
         """
         m_samples, n_features = X.shape
-        self._initialize_weights(n_features)
 
         classes_ = np.unique(y)  # 类的所有取值
         n_classes = len(classes_)
         if n_classes < 2:
             raise ValueError("At least Two classes")
 
-        if self.multi_class:
+        self._initialize_weights(n_features, n_classes)
 
-            pass
+        if self.multi_class:
+            for i in range(n_iterations):
+                loss, gradient = self._loss_and_gradient_softmax(X, y)
+                # loss, gradient = loss_grad_softmax_vectorized(self.W, X.T, y, self.reg)
+                self.W -= self.learning_rate * gradient
+                if i % 100 == 0:
+                    print("loss of iteration {0} is {1}: ".format(i, loss))
         else:
             for i in range(n_iterations):
-                loss, gradient = self._loss_and_gradient(X, y)
-                self.params -= self.learning_rate * gradient
+                loss, gradient = self._loss_and_gradient_sigmoid(X, y)
+                self.W -= self.learning_rate * gradient
                 if i % 100 == 0:
                     print("loss of iteration {0} is {1}: ".format(i, loss))
 
     def predict(self, X):
-        y_pred = sigmoid(X.dot(self.params))
-        return np.round(y_pred).astype(int)
+        if self.multi_class:
+            y_pred = softmax(X.dot(self.W.T))
+            return np.argmax(y_pred, axis=1)
+        else:
+            y_pred = sigmoid(X.dot(self.W))
+            return np.round(y_pred).astype(int)
 
 
 def sigmoid(x):
@@ -85,23 +124,30 @@ def softmax(x):
     return ex / (np.sum(ex, axis=-1, keepdims=True))
 
 
-if __name__ == '__main__':
-    """
-    下面使用iris作为二分类数据集使用 
-    """
-    from sklearn.datasets import load_iris
-    from sklearn.model_selection import train_test_split
+def binary_classify():
+    data = load_digits(2)
+    X = normalize(data.data)
+    y = data.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=True, random_state=12)
 
-    data = load_iris()
-    # 只是用 1 2 类
-    X = normalize(data.data[data.target != 0])
-    y = data.target[data.target != 0]
-    y[y == 1] = 0
-    y[y == 2] = 1
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-    clf = LogisticRegression(0.1, 0.01)
+    clf = LogisticRegression(0.01, 0.01, multi_class=False)
     clf.fit(X_train, y_train, 10000)
 
     print(accuracy_score(y_test, clf.predict(X_test)))
+
+
+def multi_classify():
+    data = load_digits(10)
+    X = normalize(data.data)
+    y = data.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=True, random_state=12)
+
+    clf = LogisticRegression(0.1, 0.01, multi_class=True)
+    clf.fit(X_train, y_train, 10000)
+
+    print(accuracy_score(y_test, clf.predict(X_test)))
+
+
+if __name__ == '__main__':
+    multi_classify()
